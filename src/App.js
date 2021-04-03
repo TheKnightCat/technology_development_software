cc00bf3b8fb5894eb6df39c105b2c0f734a36b80
TheKnightCat <60698813+TheKnightCat@users.noreply.github.com>
2021-04-03 19:49:22 +0300

    Добалены базовое представление

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import './App.css';
+import React, { useRef } from "react";
+
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
+import { Toast } from "primereact/toast";
+
+import "primereact/resources/themes/bootstrap4-light-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+
+import "./App.css";
 
 function App() {
+  const toast = useRef(null);
+  let flag = false;
+
+  function showToast() {
+    if (flag)
+      toast.current.show({
+        severity: "success",
+        summary: "Success Message",
+        life: 3000,
+      });
+    else
+      toast.current.show({
+        severity: "error",
+        summary: "Error Message",
+        life: 3000,
+      });
+    flag = !flag;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-
+        <div>
+          <InputText />
+          <Button label="Check" onClick={showToast} />
+          <Toast ref={toast} />
+        </div>
       </header>
     </div>
   );
